da8d9cfbb05d05aaef470eb04fc3b462d087d1ea
LiXiaoooowei <lixiaowei0535@gmail.com>
2017-06-09 17:08:10 +0800

    migrate Printer page to iPad

--- index.ios.js
+++ index.ios.js
@@ -5,7 +5,7 @@
 */
 
 import React, { Component } from 'react';
-import {AppRegistry,StyleSheet,Button,View, Alert, Text} from 'react-native';
+import {AppRegistry,StyleSheet,Button,View, Alert, Text, TouchableHighlight} from 'react-native';
 import * as Firebase from "firebase";
 
 var config = {
@@ -51,45 +51,69 @@ var QueueLabel = React.createClass({
   },
   render: function() {
     if (this.state.Qnum == 0) {
-      return <Text> Welcome! </Text>
+      return <Text style = {styles.labelText}> Welcome! </Text>
     } else {
-      return <Text> Your Queue Number is {this.state.Qnum}</Text>;
+      return <Text style = {styles.labelText}> Your Queue Number is {this.state.Qnum}</Text>;
     }
   }
 });
 
 export default class QsystemPrinter extends Component {
-
-  _onPressButton(){
-    var firebaseRef = FirebaseApp.database().ref();
-    var userRef = firebaseRef.child('Qlist');
-    var qtotalRef = firebaseRef.child('Qtotal');
-
-    qtotalRef.once("value",function(snapshot){
-      var currTotal = snapshot.val()+1;
-      if (currTotal == 1 ){
-        currTotal = Math.floor((6-Math.random())*100);
-        firebaseRef.update({
-          "baseNumber": currTotal-1,
-          "Qserving": currTotal-1
-        });
-      }
-      firebaseRef.update({
-        "Qtotal":currTotal
-      });
-      userRef.child(currTotal).set({
-        "queueNumber": currTotal,
-        "servedCounter": -1
-      });
-    });
+  constructor(props){
+    super(props);
+    this.state = {
+      pressed: false,
+      hi: true
+    }
+    this._showunderlay = this._showunderlay.bind(this);
+    this._hideunderlay = this._hideunderlay.bind(this);
+    this.addHandler = this.addHandler.bind(this);
   }
+  _showunderlay() {
+    this.setState({
+      pressed: true
+    })
+  }
+  _hideunderlay() {
+    this.setState({
+      pressed: false
+    })
+  }
+  addHandler() {
+		var firebaseRef = FirebaseApp.database().ref();
+		var userRef = firebaseRef.child('Qlist');
+		var qtotalRef = firebaseRef.child('Qtotal');
+
+		qtotalRef.once("value",function(snapshot){
+			var currTotal = snapshot.val()+1;
+			if (currTotal == 1 ){
+				currTotal = Math.floor((6-Math.random())*100);
+				firebaseRef.update({
+					"baseNumber": currTotal-1,
+					"Qserving": currTotal-1
+				});
+			}
+			firebaseRef.update({
+				"Qtotal":currTotal
+			});
+			userRef.child(currTotal).set({
+				"queueNumber": currTotal,
+				"servedCounter": -1
+			});
+		});
+	}
   render() {
     return (
-      <View style={styles.container}>
-      <View style = {styles.buttonContainer}>
+      <View style = {styles.container}>
+      <View style = {styles.labelContainer}>
       <QueueLabel />
-      <Button onPress = {this._onPressButton} title = "Queue" />
       </View>
+      <TouchableHighlight onPress = {this.addHandler} underlayColor = 'transparent' onShowUnderlay = {this._showunderlay}
+      onHideUnderlay = {this._hideunderlay}>
+      <View style = {this.state.pressed? styles.buttonPressed: styles.buttonNormal}>
+      <Text style = {this.state.pressed? styles.buttonTextPressed: styles.buttonTextNormal}>Queue</Text>
+      </View>
+      </TouchableHighlight>
       </View>
     );
   }
@@ -101,9 +125,36 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    flexDirection: 'column'
   },
-  buttonContainer: {
-    margin: 20
+  labelContainer: {
+    height: 150
+  },
+  labelText: {
+    fontSize: 40,
+    fontWeight: 'bold'
+  },
+  buttonNormal: {
+    backgroundColor: '#e7e7e7',
+    padding: 20,
+    borderRadius: 20,
+    borderColor: 'gray',
+    borderWidth: 2
+  },
+  buttonPressed: {
+    borderColor: 'gray',
+    backgroundColor: "#111111",
+    borderWidth: 2,
+    borderRadius: 20,
+    padding: 20
+  },
+  buttonTextNormal: {
+    fontSize: 60,
+    color: 'black'
+  },
+  buttonTextPressed: {
+    fontSize: 60,
+    color: 'white'
   },
 });
 
